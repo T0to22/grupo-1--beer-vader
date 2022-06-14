e73cc5cced9f703901c85b6a8f62d7a9e1d5419d
Giuliana Cabrera <giulicabreraa@gmail.com>
2022-06-13 21:17:39 -0300

    cambie public por views

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.static(path.resolve(__dirname, 'public'))
 );
 
 app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, './views/home.html'));
+    res.sendFile(path.resolve(__dirname, './views/addtocart.html'));
 });
 
 app.listen(4000, () => {
