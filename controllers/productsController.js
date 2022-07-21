f17094d7990a145c617cd0a64b5ce78530bb9128
Toto <marcelo@ntasistemas.com.ar>
2022-07-21 16:54:43 -0300

    Avances Sprint 4

--- controllers/productsController.js
+++ controllers/productsController.js
@@ -1,23 +1,33 @@
-const { v4: uuidv4 } = require("uuid");
-const fs = require("fs");
-const PATH = require("path");
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+const PATH = require('path');
 
 // Leo el JSON
-const productosJSON = PATH.resolve(__dirname, "../data/products.json");
-const listaDeProductosObjeto = JSON.parse(
-      fs.readFileSync(productosJSON, "utf-8")
-);
+const productosJSON = PATH.resolve(__dirname, '../data/products.json');
+const listaDeProductosObjeto = JSON.parse(fs.readFileSync(productosJSON, 'utf-8'));
 
 const productsController = {
-  productos: (req, res) => {
-    res.render("productos/productos", {
-      hojaCSS: "productos",
-      listaDeProductos: listaDeProductosObjeto,
-    });
-  },
-  abmProductos: (req, res) => {
-    res.render("admin/productos/abmProductos", { hojaCSS: "abmProductos" });
-  },
+	listaProductos: (req, res) => {
+		res.render('productos/productos', {
+			hojaCSS: 'productos',
+			listaDeProductos: listaDeProductosObjeto
+		});
+	},
+	abmProductos: (req, res) => {
+		res.render('admin/productos/abmProductos', { hojaCSS: 'abmProductos' });
+	},
+	nuevoProducto: (req, res) => {
+		const producto = {
+			id: uuidv4(),
+			nombre: req.body.nombre,
+			precio: req.body.precio,
+			descripcion: req.body.descripcion,
+			imagen: req.file.filename
+		};
+		listaDeProductosObjeto.push(producto);
+		fs.writeFileSync(productosJSON, JSON.stringify(listaDeProductosObjeto, null, ' '));
+		res.redirect('admin/productos/abmProductos');
+	}
 };
 
 module.exports = productsController;
--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const upload = require('../middlewares/multer');
 
 const productsController = require("../controllers/productsController")
 
-router.get('/productos', productsController.productos)  
+// Rutas de productos
+router.get('/productos', productsController.listaProductos)
 router.get('/admin/productos/abmProductos', productsController.abmProductos)
+
+// Alta de productos
+router.get('/nuevoProducto', upload.single('image'), productsController.nuevoProducto)
 
 module.exports = router
